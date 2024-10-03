924335f325c76e324fd5d50a20e898e57979dfd4
MJDayunot <mjanedayunot@gmail.com>
2024-10-03 18:40:09 +0800

    AddResearchCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BtsComponent} from "./component/bts/bts.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BtsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/component/bts/bts.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-bts',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './bts.component.html',
+  styleUrl: './bts.component.css'
+})
+export class BtsComponent {
+
+}
